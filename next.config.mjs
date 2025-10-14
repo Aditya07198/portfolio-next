a0074e7ee765208c7f3dde37207b11cbd887a165
Aditya07198 <adittyagaliara@gmail.com>
2025-10-14 21:53:42 +0530

    Updated the dependencies

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,9 @@
+import { fileURLToPath } from "url";
+import path from "path";
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true
+  outputFileTracingRoot: path.dirname(fileURLToPath(import.meta.url)),
 };
+
 export default nextConfig;
